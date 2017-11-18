49941e4ef4f85e7a04ff6c542de4e19239613ef2
Trois-Six <trois.six@free.fr>
2017-11-18 20:03:24 +0100

    CHG: add manifests

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,15 @@ mkdir -p .repo/local_manifests
 cat > .repo/local_manifests/roomservice.xml << __EOF__
 <?xml version="1.0" encoding="UTF-8"?>
 <manifest>
-  <project path="device/samsung/a5ultexx" name="DeadSquirrel01/android_device_samsung_a5ultexx" remote="github" revision="cm-14.1" />
-  <project path="device/samsung/a5-common" name="DeadSquirrel01/android_device_samsung_a5-common" remote="github" revision="cm-14.1" />
-  <project path="device/samsung/qcom-common" name="lineageos/android_device_samsung_qcom-common" remote="github" revision="cm-14.1" />
+  <project name="DeadSquirrel01/proprietary_vendor_samsung" path="vendor/samsung" remote="github" revision="cm-14.1" />
+  <project name="LineageOS/android_hardware_samsung" path="hardware/samsung" remote="github" revision="cm-14.1" />
+  <project name="LineageOS/android_packages_resources_devicesettings" path="packages/resources/devicesettings" remote="github" revision="cm-14.1" />
   <project path="device/qcom/common" name="lineageos/android_device_qcom_common" remote="github" revision="cm-14.1" />
-  <project path="external/stlport" name="lineageos/android_external_stlport" remote="github" revision="cm-14.1" />
+  <project path="device/samsung/a5-common" name="DeadSquirrel01/android_device_samsung_a5-common" remote="github" revision="cm-14.1" />
+  <project path="device/samsung/a5ultexx" name="DeadSquirrel01/android_device_samsung_a5ultexx" remote="github" revision="cm-14.1" />
+  <project path="device/samsung/qcom-common" name="lineageos/android_device_samsung_qcom-common" remote="github" revision="cm-14.1" />
   <project path="external/sony/boringssl-compat" name="lineageos/android_external_sony_boringssl-compat" remote="github" revision="cm-14.1" />
+  <project path="external/stlport" name="lineageos/android_external_stlport" remote="github" revision="cm-14.1" />
   <project path="kernel/samsung/msm8916" name="DeadSquirrel01/android_kernel_samsung_msm8916" remote="github" revision="cm-14.1" />
   <project path="vendor/samsung" name="DeadSquirrel01/proprietary_vendor_samsung" remote="github" revision="cm-14.1" />
 </manifest>
